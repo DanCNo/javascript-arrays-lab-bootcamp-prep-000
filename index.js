ed1a502de97b894c38d7ea66c5c42e4eca5bb7fe
Cheol Noh <dan.noh@gmail.com>
2018-08-15 22:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function appendKitten(name) {
 }
 
 function prependKitten(name) {
-  var newKittensArray = kittens
-  newKittensArray.splice(0, 0, name)
+  var newKittensArray = []
+  newKittensArray.concat(name)
   return newKittensArray
 }
 
